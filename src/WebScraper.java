07d58d44e19ade70e6812acdb173eb9bf4e44326
michaelgchen <michaelguanyichen@gmail.com>
2018-09-19 18:19:42 -0500

    Initial project commit. Got it done

--- src/WebScraper.java
+++ src/WebScraper.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class WebScraper {
     public static void main (String[] unused) {
         System.out.println("Ran");
+        System.out.println(urlToString("http://erdani.com/tdpl/hamlet.txt"));
+        String urlString = urlToString("http://erdani.com/tdpl/hamlet.txt");
+        System.out.println(countString(urlString, "Ham."));
     }
     /**
      * Retrieve contents from a URL and return them as a string.
@@ -23,5 +26,16 @@ public class WebScraper {
         String contents = urlScanner.useDelimiter("\\A").next();
         urlScanner.close();
         return contents;
+    }
+    public static int countString(final String url, String word){
+        String[] splitArray = url.split(" ");
+        int count = 0;
+        for (int i = 0; i <splitArray.length; i++) {
+            if (splitArray[i].equalsIgnoreCase(word)){
+                count++;
+            }
+        }
+
+        return count;
     }
 }
